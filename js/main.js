2b36cad53af8a13e3a1ab8f4d8c6d0dcbeb724ab
Tina <tparkmedia@gmail.com>
2018-05-14 14:54:31 -0700

    created psuedocode for game logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("JS is loaded..")
+console.log("JS is loaded..");
+
+
+// Psuedocode
+// 1. Timer should start at 30 seconds.
+// 2. When the user clicks on the start game, the timer should start decreasing by 1 second.
+// 3. Once the timer starts, the mole needs to appear.
+// 4. The mole should appear in a random hole in 1 second.
+// 5. When user clicks on a mole the score should increment by 1.
+// 6. When the time reaches 0 display alert with user's score
+// 7. click OK on alert and reset gameboard
